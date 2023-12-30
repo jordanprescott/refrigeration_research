709351aefaa1460df2cc6b41e65e1f906cb4a1f1
jordanprescott <jordanprescott@ucsb.edu>
2023-12-30 15:29:07 -0800

    updated animation

--- AnimatorEvents.py
+++ AnimatorEvents.py
@@ -2,49 +2,277 @@ from SystemEvents import *
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 from typing import List
+import matplotlib.patches as mpatches
+import numpy as np
+from matplotlib import cm
+
 
 class AnimChannel():
-    def __init__(self, channel):
+    def __init__(self, channel, pos, size):
         self.channel = channel
+        self.pos = pos
+        self.size = size
+        if pos and size:
+            self.coords = np.array([
+                [pos[0], pos[1]],
+                [pos[0] + size[0], pos[1]],
+                [pos[0] + size[0], pos[1] + size[1]],
+                [pos[0], pos[1] + size[1]],
+            ])
 
     def update(self, ax):
         raise NotImplementedError
 
+
+
+
+
 class Animator():
-    def __init__(self, animChannels: List(AnimChannel), sim):
+    def __init__(self, animChannels: List[AnimChannel], sim, xBounds, yBounds):
         self.animChannels = animChannels
         self.sim = sim
         fig, ax = plt.subplots()
         self.fig = fig
         self.ax = ax
-
+        self.xBounds = xBounds
+        self.yBounds = yBounds
 
     def animate(self, _):
+
         self.sim.step()
 
         self.ax.clear()
-        
+
         for aCh in self.animChannels:
+            self.ax.set_xlim(self.xBounds)
+            self.ax.set_ylim(self.yBounds)
             aCh.update(self.ax)
+
 
     def runAnimation(self, interval, frames, fileName=None, fps=10):
         ani = FuncAnimation(self.fig, self.animate, interval=interval, frames=frames)
-        
+
         if fileName:
             ani.save('animation.gif', writer='imagemagick', fps=fps)
         plt.show()
 
 
-#--------------------
+# --------------------
 
+
+# compressor stuff
 class Compressor(AnimChannel):
-    def __init__(self, channel, pos, size):
-        super().__init__(self, channel)
-        self.pos = pos
-        self.size = size
+    # pos will be coordinate of bottom left corner as tuple (x, y), size will be (width, length)
+    def __init__(self, channel, pos, size, numCompressors):
+        super().__init__(channel, pos, size)
+        self.numCompressors = numCompressors
+        self.cAdded = 0
 
     def update(self, ax):
-        sv = self.channel.value
+        ax.add_patch(mpatches.Polygon(self.coords, fill=False))
+        ax.text(self.pos[0] + self.size[0], self.pos[1], '- - - 0%', fontsize=10, va='center', ha='left')
+        ax.text(self.pos[0] + self.size[0], self.pos[1] + self.size[1], '- - - 100%', fontsize=10, va='center', ha='left')
+        ax.text(self.pos[0] + self.size[0], self.pos[1] + self.size[1] * 0.5, ' (Percent on)', fontsize=10, va='center', ha='left')
 
-        ax.add_patch()
+        yBuffer = self.size[1] * 0.2
 
+        for i in range(self.numCompressors):
+            ax.text(self.pos[0] + self.size[0]/self.numCompressors * 0.5 + self.size[0] * i / self.numCompressors, self.pos[1] - yBuffer, 'C'+str(i+1), fontsize=10, va='center', ha='center')
+        ax.text(self.pos[0] + self.size[0] * 0.5, self.pos[1] - 2.5 * yBuffer, 'Compressors', fontsize=10, va='center', ha='center')
+
+
+class Compressors(AnimChannel):
+    def __init__(self, channel, body):
+        super().__init__(channel, None, None)
+
+        xBuffer = body.size[0] * .05
+        yBuffer = body.size[1] * .05
+
+        effWidth = (body.size[0] - xBuffer) / body.numCompressors
+
+        self.body = body
+
+        self.coords = np.array([
+            [body.pos[0] + xBuffer + effWidth * body.cAdded, body.pos[1] + yBuffer],
+            [body.pos[0] + effWidth * (body.cAdded+1), body.pos[1] + yBuffer],
+            [body.pos[0] + effWidth * (body.cAdded+1), body.pos[1] + body.size[1] - yBuffer],
+            [body.pos[0] + xBuffer + effWidth * body.cAdded, body.pos[1] + body.size[1] - yBuffer]
+        ])
+
+        self.slideCoords = np.array([
+            [body.pos[0] + xBuffer + effWidth * body.cAdded, body.pos[1] + yBuffer],
+            [body.pos[0] + effWidth * (body.cAdded + 1), body.pos[1] + yBuffer],
+            [body.pos[0] + effWidth * (body.cAdded + 1), body.pos[1] + self.channel.value * (body.size[1] - yBuffer)],
+            [body.pos[0] + xBuffer + effWidth * body.cAdded, body.pos[1] + self.channel.value * (body.size[1] - yBuffer)]
+        ])
+
+        body.cAdded += 1
+
+    def update(self, ax):
+        print(self.channel.value)
+        if self.channel.value != -1:
+            self.slideCoords[2][1] = self.body.pos[1] + self.channel.value * self.body.size[1] * 0.95
+            self.slideCoords[3][1] = self.body.pos[1] + self.channel.value * self.body.size[1] * 0.95
+            ax.add_patch(mpatches.Polygon(self.slideCoords, fill=True))
+        ax.add_patch(mpatches.Polygon(self.coords, fill=False, color='black'))
+
+
+
+# evaporator stuff
+class Evaporator(AnimChannel):
+    def __init__(self, channel, pos, size, numEvaporators):
+        super().__init__(channel, pos, size)
+        self.numEvaporators = numEvaporators
+        self.eAdded = 0
+
+    def update(self, ax):
+        ax.add_patch(mpatches.Polygon(self.coords, fill=False))
+
+        yBuffer = self.size[1] * 0.2
+
+        ax.text(self.pos[0] + self.size[0] * 0.5, self.pos[1] - 2.5 * yBuffer, 'Evaporators', fontsize=10, va='center', ha='center')
+
+
+
+class Evaporators(AnimChannel):
+    def __init__(self, channel, body):
+        super().__init__(channel, None, None)
+
+        xBuffer = body.size[0] * .05
+        yBuffer = body.size[1] * .07
+
+        effWidth = (body.size[0] - xBuffer) / body.numEvaporators
+
+        self.coords = np.array([
+            [body.pos[0] + xBuffer + effWidth * body.eAdded, body.pos[1] + yBuffer],
+            [body.pos[0] + effWidth * (body.eAdded + 1), body.pos[1] + yBuffer],
+            [body.pos[0] + effWidth * (body.eAdded + 1), body.pos[1] + body.size[1] - yBuffer],
+            [body.pos[0] + xBuffer + effWidth * body.eAdded, body.pos[1] + body.size[1] - yBuffer]
+        ])
+
+        body.eAdded += 1
+
+    def update(self, ax):
+        ax.add_patch(mpatches.Polygon(self.coords, fill=self.channel.value, edgecolor='black'))
+
+
+# vessel stuff
+class Vessel(AnimChannel):
+    def __init__(self, channel, pos, size):
+        super().__init__(channel, pos, size)
+
+    def update(self, ax):
+        ax.add_patch(mpatches.Polygon(self.coords, fill=False))
+        plt.text(self.pos[0] + self.size[0]/2, self.pos[1] + self.size[1] * .75, 'Vessel', fontsize=10, va='center', ha='center')
+        plt.text(self.pos[0] + self.size[0]/2, self.pos[1] + self.size[1] * .25, 'SP:' + str(round(self.channel.value, 5)), fontsize=10, va='center', ha='center')
+
+
+# condenser stuff
+class Condenser(AnimChannel):
+    def __init__(self, channel, pos, size):
+        super().__init__(channel, pos, size)
+
+    def update(self, ax):
+        ax.add_patch(mpatches.Polygon(self.coords, fill=False))
+        plt.text(self.pos[0] + self.size[0] * 0.5, self.pos[1] + self.size[1] * 0.5, 'Condenser', fontsize=10, va='center', ha='center')
+
+
+# expansion valve stuff
+class ExpansionValve(AnimChannel):
+    def __init__(self, channel, pos, size):
+        super().__init__(channel, pos, size)
+        self.coords = np.insert(self.coords, 0, [[pos[0] + size[0] * 0.5, pos[1] + size[1] * 0.5]], axis=0)
+        self.coords = np.insert(self.coords, 3, [[pos[0] + size[0] * 0.5, pos[1] + size[1] * 0.5]], axis=0)
+
+
+
+    def update(self, ax):
+        ax.add_patch(mpatches.Polygon(self.coords, fill=True, color='black'))
+        plt.text(self.pos[0] + self.size[0], self.pos[1] + self.size[1] * 0.5, 'Expansion Valve', fontsize=10, va='center', ha='left')
+
+
+# q_out stuff
+class HeatOut(AnimChannel):
+    def __init__(self, channel, pos):
+        super().__init__(channel, pos, None)
+
+    def update(self, ax):
+        inferno_cm = (cm.inferno(range(256)))
+        scale = 0.00045  # use to normalize qin and qout, tweak internally
+
+        ax.arrow(self.pos[0], self.pos[1], 0, self.channel.value * scale, shape='full', linewidth=3, head_width=.03,
+                 color=inferno_cm[round(self.channel.value * scale * 255)])
+        plt.text(self.pos[0], self.pos[1] + self.channel.value * scale * 0.5, '  Q out', fontsize=10, va='center', ha='left')
+
+
+# q_in stuff
+class HeatIn(AnimChannel):
+    def __init__(self, channel, pos):
+        super().__init__(channel, pos, None)
+
+    def update(self, ax):
+        inferno_cm = cm.inferno(range(256))
+        scale = 0.00045  # use to normalize qin and qout, tweak internally
+
+        ax.arrow(self.pos[0], self.pos[1] - self.channel.value * scale, 0, self.channel.value * scale, shape='full', linewidth=3, head_width=.03,
+             color=inferno_cm[round(self.channel.value * scale * 255)], label='1')
+        plt.text(self.pos[0], self.pos[1] - self.channel.value * scale * 0.5, '  Q in', fontsize=10, va='center', ha='left')
+
+
+class PathArrows(AnimChannel):
+    def __init__(self, vessel, compressor, condenser, expansion, evaporator):
+        super().__init__(None, None, None)
+        self.vessel = vessel
+        self.compressor = compressor
+        self.condenser = condenser
+        self.expansion = expansion
+        self.evaporator = evaporator
+
+    def update(self, ax):
+        ax.arrow(self.vessel.pos[0] + self.vessel.size[0], self.vessel.pos[1] + self.vessel.size[1] * 0.5,
+                 (self.compressor.pos[0] - (self.vessel.pos[0] + self.vessel.size[0])), 0, shape='full',
+                 linewidth=1.2,
+                 head_width=0.0, color='black')
+        ax.arrow(self.vessel.pos[0]+self.vessel.size[0], self.vessel.pos[1] + self.vessel.size[1] * 0.5,
+                 (self.compressor.pos[0]-(self.vessel.pos[0]+self.vessel.size[0]))/2, 0, shape='full', linewidth=0.8,
+                 head_width=0.03, color='black')
+
+        ax.arrow(self.compressor.pos[0] + self.compressor.size[0] * 0.5, self.compressor.pos[1] + self.compressor.size[1],
+                 0, self.condenser.pos[1] - ( self.compressor.pos[1] + self.compressor.size[1]), shape='full', linewidth=1.2,
+                 head_width=0.0, color='black')
+        ax.arrow(self.compressor.pos[0] + self.compressor.size[0] * 0.5,
+                 self.compressor.pos[1] + self.compressor.size[1],
+                 0, (self.condenser.pos[1] - (self.compressor.pos[1] + self.compressor.size[1]))/2, shape='full',
+                 linewidth=0.8,
+                 head_width=0.02, color='black')
+
+        ax.arrow(self.condenser.pos[0], self.condenser.pos[1] + self.condenser.size[1] * 0.5,
+                 (self.expansion.pos[0] + self.expansion.size[0] * 0.5) - self.condenser.pos[0], 0, shape='full',
+                 linewidth=1.2, head_width=0.0, color='black')
+        ax.arrow(self.condenser.pos[0], self.condenser.pos[1] + self.condenser.size[1] * 0.5,
+                 ((self.expansion.pos[0] + self.expansion.size[0] * 0.5) - self.condenser.pos[0])/2, 0, shape='full',
+                 linewidth=1.2, head_width=0.02, color='black')
+        ax.arrow(self.expansion.pos[0] + self.expansion.size[0] * 0.5, self.condenser.pos[1] + self.condenser.size[1] * 0.5,
+                  0, self.expansion.pos[1]+self.expansion.size[1] - (self.condenser.pos[1] + self.condenser.size[1] * 0.5),
+                 shape='full', linewidth=1.2, head_width=0.02, color='black')
+
+        ax.arrow(self.expansion.pos[0] + self.expansion.size[0] * 0.5, self.expansion.pos[1], 0,
+                 self.vessel.pos[1] + self.vessel.size[1] - self.expansion.pos[1], shape='full', linewidth=1.2,
+                 head_width=0.0, color='black')
+        ax.arrow(self.expansion.pos[0] + self.expansion.size[0] * 0.5, self.expansion.pos[1], 0,
+                 (self.vessel.pos[1] + self.vessel.size[1] - self.expansion.pos[1])/2, shape='full', linewidth=1.2,
+                 head_length=0.05, head_width=0.015, color='black')
+
+        ax.arrow(self.vessel.pos[0] + self.vessel.size[0] * 0.25, self.vessel.pos[1], 0,
+                 self.evaporator.pos[1] + self.evaporator.size[1] - self.vessel.pos[1], shape='full', linewidth=1.2,
+                 head_width=0.0, color='black')
+        ax.arrow(self.vessel.pos[0] + self.vessel.size[0] * 0.25, self.vessel.pos[1], 0,
+                 (self.evaporator.pos[1] + self.evaporator.size[1] - self.vessel.pos[1]) * 0.5, shape='full', linewidth=1.2,
+                head_width=0.015, color='black')
+
+        ax.arrow(self.vessel.pos[0] + self.vessel.size[0] * 0.75, self.vessel.pos[1], 0,
+                 self.evaporator.pos[1] + self.evaporator.size[1] - self.vessel.pos[1], shape='full', linewidth=1.2,
+                head_width=0.0, color='black')
+        ax.arrow(self.vessel.pos[0] + self.vessel.size[0] * 0.75, self.evaporator.pos[1] + self.evaporator.size[1], 0,
+                 -(self.evaporator.pos[1] + self.evaporator.size[1] - self.vessel.pos[1]) * 0.5, shape='full', linewidth=1.2,
+                 head_width=0.015, color='black')
--- RefrigeratorSimulator.py
+++ RefrigeratorSimulator.py
@@ -1,151 +1,204 @@
 from SystemEvents import *
+from AnimatorEvents import *
 import numpy as np
 import CoolProp.CoolProp as CP
 
 ### CHANNELS
 
 channels = [
-    DataChannel('Time', 0),  # seconds
-    DataChannel('DeltaT', .05),  # seconds
-    DataChannel('RoomTemp', 0),  # Celsius
-    DataChannel('AmbientTemp', 22),  # Celsius
-    DataChannel('SuctionPressure', 18),  # psia
-    DataChannel('DischargePressure', 180),  # psia
-    DataChannel('TotalCompressorPower', 300),  # kW
-    DataChannel('Qadded', 600),  # kW
-    DataChannel('TotalQin', 600),  # kW
-    DataChannel('TotalQout', 1000),  # kW
-    DataChannel('TotalVolumetricHighFlow', .5),  # m^3/s
-    DataChannel('TotalMassHighFlow', 1),  # kg/s
-    DataChannel('TotalMassLowFlow', 1),  # kg/s
-    DataChannel('SlideValveA', -1),  # -1 signifying off, on in [0, 1]
-    DataChannel('SlideValveB', -1),  # -1 signifying off, on in [0, 1]
-    DataChannel('SlideValveC', -1),  # -1 signifying off, on in [0, 1]
-    DataChannel('SlideValveD', -1),  # -1 signifying off, on in [0, 1]
-    DataChannel('EvaporatorA', 0),  # state of evaporators, {0, 1}
-    DataChannel('EvaporatorB', 0),  # state of evaporators, {0, 1}
-    DataChannel('EvaporatorC', 0),  # state of evaporators, {0, 1}
-    DataChannel('EvaporatorD', 0),  # state of evaporators, {0, 1}
-    DataChannel('EvaporatorE', 0),  # state of evaporators, {0, 1}
-    DataChannel('TotalEvapDuty', 0),  # total evaporator duty
-    DataChannel('CondenserFan', .5),  # condenser  duty
-    DataChannel('alpha', 8), # Transfer Coeff for Qin
-    DataChannel('beta', 200),  # Transfer Coeff for Qout
+	DataChannel('Time', 0),  # seconds
+	DataChannel('DeltaT', .05),  # seconds
+	DataChannel('RoomTemp', 0),  # Celsius
+	DataChannel('AmbientTemp', 22),  # Celsius
+	DataChannel('SuctionPressure', 18),  # psia
+	DataChannel('DischargePressure', 180),  # psia
+	DataChannel('TotalCompressorPower', 300),  # kW
+	DataChannel('Qadded', 600),  # kW
+	DataChannel('TotalQin', 600),  # kW
+	DataChannel('TotalQout', 1000),  # kW
+	DataChannel('TotalVolumetricHighFlow', .5),  # m^3/s
+	DataChannel('TotalMassHighFlow', 1),  # kg/s
+	DataChannel('TotalMassLowFlow', 1),  # kg/s
+	DataChannel('SlideValveA', -1),  # -1 signifying off, on in [0, 1]
+	DataChannel('SlideValveB', -1),  # -1 signifying off, on in [0, 1]
+	DataChannel('SlideValveC', -1),  # -1 signifying off, on in [0, 1]
+	DataChannel('SlideValveD', -1),  # -1 signifying off, on in [0, 1]
+	DataChannel('EvaporatorA', 0),  # state of evaporators, {0, 1}
+	DataChannel('EvaporatorB', 0),  # state of evaporators, {0, 1}
+	DataChannel('EvaporatorC', 0),  # state of evaporators, {0, 1}
+	DataChannel('EvaporatorD', 0),  # state of evaporators, {0, 1}
+	DataChannel('EvaporatorE', 0),  # state of evaporators, {0, 1}
+	DataChannel('TotalEvapDuty', 0),  # total evaporator duty
+	DataChannel('CondenserFan', .5),  # condenser  duty
+	DataChannel('alpha', 8),  # Transfer Coeff for Qin
+	DataChannel('beta', 200),  # Transfer Coeff for Qout
 
 ]
+
 
 ## Helper Functions
 
 def toPASCAL(psia): return 6894.76 * psia
 
+
 def toCelsius(Kelvin): return Kelvin - 273.15
+
 
 ### Systems
 
 def euler(x, xdot, dT): return x + xdot * dT
 
+
 def timer(Time, DeltaT): return Time + DeltaT
 
-def setQadded(Time): 
-    noise = 50 * np.cos(2 * np.pi * Time / 10 ) + 50 * np.random.random()
-    return 600 + noise
+
+def setQadded(Time):
+	noise = 50 * np.cos(2 * np.pi * Time / 10) + 50 * np.random.random()
+	return 600 + noise
+
 
 def setRoomTemp(RoomTemp, Qadded, TotalQin, DeltaT):
-    TRANSFERCOEFF = .005
-    RoomTempDot = TRANSFERCOEFF * (Qadded - TotalQin)
-    return euler(RoomTemp, RoomTempDot, DeltaT)
+	TRANSFERCOEFF = .005
+	RoomTempDot = TRANSFERCOEFF * (Qadded - TotalQin)
+	return euler(RoomTemp, RoomTempDot, DeltaT)
+
 
 def setTotalVolumetricHighFlow(SlideValveA, SlideValveB, SlideValveC, SlideValveD):
-    slides = [SlideValveA, SlideValveB, SlideValveC, SlideValveD]
-    slideOn = [e for e in slides if e > 0]
-    return (np.sum(slideOn) * .9 + len(slideOn) * .1)/len(slides)  # start up throughput around 10%
+	slides = [SlideValveA, SlideValveB, SlideValveC, SlideValveD]
+	slideOn = [e for e in slides if e > 0]
+	return (np.sum(slideOn) * .9 + len(slideOn) * .1) / len(slides)  # start up throughput around 10%
+
 
 def setTotalEvapDuty(EvaporatorA, EvaporatorB, EvaporatorC, EvaporatorD, EvaporatorE):
-    return np.sum([EvaporatorA, EvaporatorB, EvaporatorC, EvaporatorD, EvaporatorE])
+	return np.sum([EvaporatorA, EvaporatorB, EvaporatorC, EvaporatorD, EvaporatorE])
+
 
 def setTotalMassHighFlow(SuctionPressure, TotalVolumetricHighFlow):
-    return CP.PropsSI('D', 'P', toPASCAL(SuctionPressure), 'Q', 1, 'Ammonia') * TotalVolumetricHighFlow
+	return CP.PropsSI('D', 'P', toPASCAL(SuctionPressure), 'Q', 1, 'Ammonia') * TotalVolumetricHighFlow
+
 
 def setTotalQin(alpha, TotalMassLowFlow, TotalEvapDuty, RoomTemp, SuctionPressure):
-    tempDiff = RoomTemp - toCelsius(CP.PropsSI('T', 'P', toPASCAL(SuctionPressure), 'Q', 1, 'Ammonia'))
-    return alpha * TotalMassLowFlow * TotalEvapDuty * tempDiff
+	tempDiff = RoomTemp - toCelsius(CP.PropsSI('T', 'P', toPASCAL(SuctionPressure), 'Q', 1, 'Ammonia'))
+	return alpha * TotalMassLowFlow * TotalEvapDuty * tempDiff
+
 
 def setTotalQout(beta, TotalMassHighFlow, CondenserFan, AmbientTemp, DischargePressure):
-    tempDiff = toCelsius(CP.PropsSI('T', 'P', toPASCAL(DischargePressure), 'Q', 1, 'Ammonia')) - AmbientTemp
-    return beta * TotalMassHighFlow * CondenserFan * tempDiff
+	tempDiff = toCelsius(CP.PropsSI('T', 'P', toPASCAL(DischargePressure), 'Q', 1, 'Ammonia')) - AmbientTemp
+	return beta * TotalMassHighFlow * CondenserFan * tempDiff
+
 
 def setTotalCompressorPower(SlideValveA, SlideValveB, SlideValveC, SlideValveD, SuctionPressure):
-    slides = [SlideValveA, SlideValveB, SlideValveC, SlideValveD]
-    slideOn = [e for e in slides if e > 0]
-    power = np.sum(slideOn) * 100 + len(slideOn) * 75  # rough estimate of Compressor Power 
-    perc = 1 + (18 - SuctionPressure)/50 # 2%/psig efficiency gain with higher suction pressure
-    return perc * power
+	slides = [SlideValveA, SlideValveB, SlideValveC, SlideValveD]
+	slideOn = [e for e in slides if e > 0]
+	power = np.sum(slideOn) * 100 + len(slideOn) * 75  # rough estimate of Compressor Power
+	perc = 1 + (18 - SuctionPressure) / 50  # 2%/psig efficiency gain with higher suction pressure
+	return perc * power
+
 
 def setSuctionPressure(SuctionPressure, TotalVolumetricHighFlow, TotalQin, DeltaT):
-    DECAY = .5
-    VF = TotalVolumetricHighFlow
-    Q = TotalQin
-    eqSP = 15 * (1-VF)**4 + Q * (1.5 - VF)/80 + 8
-    spDot = DECAY * (eqSP - SuctionPressure)
-    return euler(SuctionPressure, spDot, DeltaT)
+	DECAY = .5
+	VF = TotalVolumetricHighFlow
+	Q = TotalQin
+	eqSP = 15 * (1 - VF) ** 4 + Q * (1.5 - VF) / 80 + 8
+	spDot = DECAY * (eqSP - SuctionPressure)
+	return euler(SuctionPressure, spDot, DeltaT)
 
 
 ## Feedback Controllers
 
 def feedbackSlideValves(SuctionPressure):
-    sp = SuctionPressure
-    if sp < 10:
-        duty = 0
-    elif sp < 30:
-        duty = (sp - 10)/20
-    else:
-        duty = 1
+	sp = SuctionPressure
+	if sp < 10:
+		duty = 0
+	elif sp < 30:
+		duty = (sp - 10) / 20
+	else:
+		duty = 1
 
-    slides = [-1, -1, -1, -1]
-    
-    for i, _ in enumerate(slides):
-        if duty > .025:
-            slides[i] = min(1, (duty-.025)/.1)
-            duty = duty - slides[i]*.225 - .025
-    
-    return slides
+	slides = [-1, -1, -1, -1]
+
+	for i, _ in enumerate(slides):
+		if duty > .025:
+			slides[i] = min(1, (duty - .025) / .1)
+			duty = duty - slides[i] * .225 - .025
+
+	return slides
+
 
 def feedbackEvaporatorOn(RoomTemp):
-    if RoomTemp < -.5:
-        N = 0
-    else:
-        N = 5 * (RoomTemp  + .5)
-    
-    return [1 if i < N else 0 for i in range(5)]
+	if RoomTemp < -.5:
+		N = 0
+	else:
+		N = 5 * (RoomTemp + .5)
+
+	return [1 if i < N else 0 for i in range(5)]
+
 
 # Optimization Controllers
 
 def optSlideValves():
-    pass
+	pass
+
 
 def optEvaporatorOn():
-    pass
+	pass
+
 
 ## Configure Systems
 
 posters = [
-    ['timer', ['Time', 'DeltaT'], ['Time'], timer],
-    func2Poster('Qadded', setQadded),
-    func2Poster('RoomTemp', setRoomTemp),
-    func2Poster('TotalVolumetricHighFlow', setTotalVolumetricHighFlow),
-    func2Poster('TotalEvapDuty', setTotalEvapDuty),
-    func2Poster('TotalMassHighFlow', setTotalMassHighFlow),
-    func2Poster('TotalQin', setTotalQin),
-    func2Poster('TotalQout', setTotalQout),
-    func2Poster('TotalCompressorPower', setTotalCompressorPower),
-    func2Poster('SuctionPressure', setSuctionPressure),
-    ['SlideValveSys', ['SuctionPressure'], ['SlideValveA', 'SlideValveB', 'SlideValveC', 'SlideValveD'], feedbackSlideValves],
-    ['EvaporatorSys', ['RoomTemp'], ['EvaporatorA', 'EvaporatorB', 'EvaporatorC', 'EvaporatorD', 'EvaporatorE'], feedbackEvaporatorOn]
+	['timer', ['Time', 'DeltaT'], ['Time'], timer],
+	func2Poster('Qadded', setQadded),
+	func2Poster('RoomTemp', setRoomTemp),
+	func2Poster('TotalVolumetricHighFlow', setTotalVolumetricHighFlow),
+	func2Poster('TotalEvapDuty', setTotalEvapDuty),
+	func2Poster('TotalMassHighFlow', setTotalMassHighFlow),
+	func2Poster('TotalQin', setTotalQin),
+	func2Poster('TotalQout', setTotalQout),
+	func2Poster('TotalCompressorPower', setTotalCompressorPower),
+	func2Poster('SuctionPressure', setSuctionPressure),
+	['SlideValveSys', ['SuctionPressure'], ['SlideValveA', 'SlideValveB', 'SlideValveC', 'SlideValveD'],
+	 feedbackSlideValves],
+	['EvaporatorSys', ['RoomTemp'], ['EvaporatorA', 'EvaporatorB', 'EvaporatorC', 'EvaporatorD', 'EvaporatorE'],
+	 feedbackEvaporatorOn]
 ]
 
-### Run Simulation
+## Run Simulation
 refSim = TimedSimulation(posters, channels)
 refSim.runSim(20)
-refSim.plotVals([['SlideValveA', 'SlideValveB', 'SlideValveC', 'SlideValveD'], ['TotalCompressorPower'], ['SuctionPressure'], ['RoomTemp'], ['Qadded', 'TotalQin']])
+refSim.plotVals(
+	[['SlideValveA', 'SlideValveB', 'SlideValveC', 'SlideValveD'], ['TotalCompressorPower'], ['SuctionPressure'],
+	 ['RoomTemp'], ['Qadded', 'TotalQin']])
 
 #################
+testSim = BaseSimulation(posters, channels)
+
+cBody = Compressor(channel=None, pos=(0.6,0.05), size=(0.25,0.5), numCompressors=4)
+ca = Compressors(channels[13], cBody)
+cb = Compressors(channels[14], cBody)
+cc = Compressors(channels[15], cBody)
+cd = Compressors(channels[16], cBody)
+
+eBody = Evaporator(channel=None, pos=(.15, -.15), size=(.3, .3), numEvaporators=5)
+ea = Evaporators(channels[17], eBody)
+eb = Evaporators(channels[18], eBody)
+ec = Evaporators(channels[19], eBody)
+ed = Evaporators(channels[20], eBody)
+ee = Evaporators(channels[21], eBody)
+
+v = Vessel(channel=channels[4], pos=(.22, .3), size=(.2, .2))
+
+cond = Condenser(channel=None, pos=(.6, 0.7), size=(0.25, 0.1))
+
+expn = ExpansionValve(channel=None, pos=(.3, 0.625), size=(0.03, 0.07))
+
+qin = HeatIn(channel=channels[8], pos=(0.3, -0.425))
+
+qout = HeatOut(channel=channels[9], pos=(0.725, 0.85))
+
+arw = PathArrows(v, cBody, cond, expn, eBody)
+
+
+a = Animator([cBody, ca, cb, cc, cd, eBody, ea, eb, ec, ed, ee, v, cond, expn, qin, qout, arw], testSim, [0.1, 1], [-0.85, 1.2])
+a.animate(10)
+a.runAnimation(100, 20)
